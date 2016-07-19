b625bda62c5aa7990b129413a8166a1ec936f547
Caidao Zhang <zjxcdut@gmail.com>
2016-07-19 19:56:59 +0800

    flex-grid

--- source/gulpfile.js
+++ source/gulpfile.js
@@ -2,12 +2,20 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var prefix = require('gulp-autoprefixer');
 var wrap = require('gulp-wrap');
-
+var browserSync = require('browser-sync');
 
 function handleError(err) {
 	console.log(err.toString());
 	this.emit('end');
 }
+
+gulp.task('browser-sync',['sass','build'], function(){
+	browserSync({
+		server:{
+			baseDir: '..'
+		}
+	});
+});
 
 gulp.task('build', function(){
 	gulp.src("pages/*.html")
@@ -19,12 +27,22 @@ gulp.task('sass', function(){
 	gulp.src('styles/main.scss')
 		.pipe(sass()).on('error', handleError)
 		.pipe(prefix())
-		.pipe(gulp.dest('../styles'));
+		.pipe(gulp.dest('../styles'))
+		.pipe(browserSync.reload({stream:true}));
+});
+
+gulp.task('cp', function(){
+	return gulp.src(['js/main.js','assets/*'],{base:'.'})
+		.pipe(gulp.dest('..'));
+});
+
+gulp.task('rebuild', ['build'], function(){
+	browserSync.reload();
 });
 
 gulp.task('watch', function(){
-	gulp.watch(['*/*.html'],['build']);
+	gulp.watch(['*/*.html'],['rebuild']);
 	gulp.watch(['styles/*.scss'],['sass']);
 });
 
-gulp.task('default', ['sass','build','watch']);
+gulp.task('default', ['cp', 'browser-sync','watch']);
